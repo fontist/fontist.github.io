8039649b532cc57881be49f26c78100399237717
jcbhmr <jcbhmr@outlook.com>
2024-02-27 15:00:06 -0600

    save

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,16 +2,13 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  // FIXME
-  ignoreDeadLinks: true,
-
   // https://vitepress.dev/guide/routing#generating-clean-url
-  // ✅ Works with GitHub Pages.
   cleanUrls: true,
 
   title: "Fontist",
   description: "Install openly-licensed fonts on Windows, Linux and Mac!",
 
+  // https://github.com/vuejs/vitepress/issues/3508
   base: process.env.BASE_PATH,
 
   themeConfig: {
@@ -19,8 +16,16 @@ export default defineConfig({
 
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: "Fontist", link: "https://fontist.org/fontist/", target: "_self" },
-      { text: "Formulas", link: "https://fontist.org/formulas/", target: "_self" },
+      {
+        text: "Fontist",
+        link: "https://fontist.org/fontist/",
+        target: "_self",
+      },
+      {
+        text: "Formulas",
+        link: "https://fontist.org/formulas/",
+        target: "_self",
+      },
       { text: "Blog", link: "/blog/" },
     ],
 
@@ -29,7 +34,7 @@ export default defineConfig({
     socialLinks: [
       { icon: "github", link: "https://github.com/fontist/fontist.github.io" },
     ],
-    
+
     footer: {
       message: `Fontist is <a href="https://open.ribose.com/">riboseopen</a>`,
       copyright: `Copyright &copy; 2023 Ribose Group Inc. All rights reserved.`,
